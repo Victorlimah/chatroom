a705608dd90d53307c537aa6c1f32a2b52a09eb6
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-02 00:26:54 -0300

    Feat: Implements ParticipantModel

---
+++ src/main/java/br/dcx/ufpb/chatroom/models/ParticipantModel.java
@@ -0,0 +1,28 @@
+package br.dcx.ufpb.chatroom.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paticipants")
+public class ParticipantModel implements Serializable{
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(nullable = false, unique = true)
+  private String name;
+
+  @Column(nullable = false)
+  private LocalDateTime lastActivity;
+}
